1875ea6148f2be402e625c4979b15abc74e55b56
Adegboyega <gboyega.banjo@gmail.com>
2022-01-12 20:41:22 +0100

    sellers board v2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./styles/index.css"
 import ProductDetails from "./views/productDetails"
+import SellersBoard from './views/sellersBoard'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ProductDetails />
+    <SellersBoard />
   </React.StrictMode>,
   document.getElementById("root")
 )
--- src/views/sellersBoard.js
+++ src/views/sellersBoard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SellersBoard() {
     return (
-        <div className='font-poppins grid grid-cols-6'>
+        <div className='font-poppins grid grid-cols-6 bg-fixed' style={{backgroundImage: url("../assets/seller-bg.png")}}>
             <div className="col-span-1"></div>
             <div className="col-span-5"></div>
             
