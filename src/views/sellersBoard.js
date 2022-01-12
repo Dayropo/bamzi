b7fd219159b8c2c2ed776d4d45a8a60f84653aa9
Adegboyega <gboyega.banjo@gmail.com>
2022-01-12 19:38:22 +0100

    sellers board v1

---
+++ src/views/sellersBoard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SellersBoard() {
+    return (
+        <div className='font-poppins grid grid-cols-6'>
+            <div className="col-span-1"></div>
+            <div className="col-span-5"></div>
+            
+        </div>
+    )
+}
